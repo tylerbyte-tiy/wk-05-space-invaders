2236e1f844cafb931b5206cda0b88b7dd0a77fec
Tyler Hamlett <tylerbyte@users.noreply.github.com>
2017-03-28 17:04:21 -0600

    feat (main) Add sound to shooting

--- game.js
+++ game.js
@@ -7,6 +7,9 @@
     this.bodies = createInvaders(this).concat(new Player(this, gameSize));
 
     var self = this;
+    loadSound("shoot.wav", function(shootSound) {
+      self.shootSound = shootSound;
+    })
     var tick = function() {
       self.update();
       self.draw(screen, gameSize);
@@ -64,6 +67,8 @@
       if (this.keyboarder.isDown(this.keyboarder.KEYS.SPACE)) {
         var bullet = new Bullet({ x: this.center.x, y: this.center.y - this.size.x * 2}, { x: 0, y: -6 });
         this.game.addBody(bullet);
+        this.game.shootSound.load();
+        this.game.shootSound.play();
       }
     }
   };
@@ -153,6 +158,18 @@ var createInvaders = function(game) {
 
     b1.center.y - b1.size.y / 2 > b2.center.y + b2.size.y /2) ;
   }
+
+  var loadSound = function(url, callback) {
+    var loaded = function() {
+
+    callback(sound);
+    sound.removeEventListener('cancanplaythrough', loaded);
+  };
+
+    var sound = new Audio(url);
+    sound.addEventListener('canplaythrough', loaded);
+    sound.load();
+  };
 
   window.onload = function() {
     new Game("screen");
