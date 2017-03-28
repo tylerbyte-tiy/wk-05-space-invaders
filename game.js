221cc7488a4c658c0b73d0050e2dab94a4db4994
Tyler Hamlett <tylerbyte@users.noreply.github.com>
2017-03-28 15:35:11 -0600

    feat (main) Add bullets

--- game.js
+++ game.js
@@ -28,7 +28,9 @@
       for (var i = 0; i < this.bodies.length; i++) {
         drawRect(screen, this.bodies[i])
       }
-
+    },
+    addBody: function(body) {
+      this.bodies.push(body);
     }
   };
   var Player = function(game, gameSize) {
@@ -45,8 +47,29 @@
       } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
         this.center.x += 2;
       }
+
+      if (this.keyboarder.isDown(this.keyboarder.KEYS.SPACE)) {
+        var bullet = new Bullet({ x: this.center.x, y: this.center.y - this.size.x * 2}, { x: 0, y: -6 });
+        this.game.addBody(bullet);
+      }
     }
   };
+
+///////////////////////////////////////////
+var Bullet = function(center, velocity) {
+  this.size = { x: 3, y: 3 };
+  this.center = center;
+  this.velocity = velocity;
+};
+
+Bullet.prototype = {
+  update: function() {
+    this.center.x += this.velocity.x;
+    this.center.y += this.velocity.y;
+
+  }
+};
+///////////////////////////////////////////
 
   var drawRect = function(screen, body) {
     screen.fillRect(body.center.x - body.size.x / 2, body.center.y - body.size.y / 2, body.size.x, body.size.y);
